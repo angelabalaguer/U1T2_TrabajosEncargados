62ca10cf793ec00ecb2fdb0e6569000afe0c640d
Angela Balaguer <angbalaguerv@upt.pe>
2020-01-16 08:53:10 -0500

    initial commit

---
+++ app/src/main/java/com/example/u1t2trabajosencargados/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.u1t2trabajosencargados;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
