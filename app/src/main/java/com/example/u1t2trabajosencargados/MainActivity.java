0b1c084971abcdfa617b7baba72714821a4cba7f
Angela Balaguer <angbalaguerv@upt.pe>
2020-01-16 12:20:00 -0500

    DISEÑO LOGIN Y CREACION DE ACTIVIDADES

--- app/src/main/java/com/example/u1t2trabajosencargados/MainActivity.java
+++ app/src/main/java/com/example/u1t2trabajosencargados/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.u1t2trabajosencargados;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Musica(View view) {
+        startActivity(new Intent(this, Ejercicio1.class));
+    }
+
+    public void Login(View view) {
+        startActivity(new Intent(this, Ejercicio2.class));
     }
 }
